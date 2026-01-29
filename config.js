b9623b463b5f00e951b4070e99721e65cefcf9f4
Ruslan Chmir <ruslan.chmir@panda-ventures.net>
2026-01-29 13:08:49 +0100

    added base site functionality

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration file
+// This file can be generated from .env file using: node generate-config.js
+// Or you can manually edit it
+// 
+// SECURITY NOTE: This URL will be visible in browser DevTools.
+// For production, add authentication to your n8n webhook.
+
+window.APP_CONFIG = {
+  N8N_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/18bfde36-fd7d-48c9-a257-64ee9cfab615"
+};
