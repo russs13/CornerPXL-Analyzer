032833cdc6674f6f6bfb48539fe1a397c573175b
Ruslan Chmir <ruslan.chmir@panda-ventures.net>
2026-01-30 12:17:34 +0100

    new logic with downloading

--- config.js
+++ config.js
@@ -6,5 +6,6 @@
 // For production, add authentication to your n8n webhook.
 
 window.APP_CONFIG = {
-  N8N_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/18bfde36-fd7d-48c9-a257-64ee9cfab615"
+  N8N_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/18bfde36-fd7d-48c9-a257-64ee9cfab615",
+  N8N_DOWNLOAD_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/6fca094c-0db6-4d2e-b602-4221addf6035"
 };
