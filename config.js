8ab45409cdce95156710f5fbe719c4c33866a85c
Ruslan Chmir <ruslan.chmir@panda-ventures.net>
2026-02-11 10:13:14 +0100

    update configuration to production urls

--- config.js
+++ config.js
@@ -6,6 +6,8 @@
 // For production, add authentication to your n8n webhook.
 
 window.APP_CONFIG = {
-  N8N_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/18bfde36-fd7d-48c9-a257-64ee9cfab615",
-  N8N_DOWNLOAD_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/6fca094c-0db6-4d2e-b602-4221addf6035"
+  // N8N_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/18bfde36-fd7d-48c9-a257-64ee9cfab615", // DEV
+  // N8N_DOWNLOAD_WEBHOOK_URL: "https://n8n-dev.mindtech.studio/webhook/6fca094c-0db6-4d2e-b602-4221addf6035" // DEV
+  N8N_WEBHOOK_URL: "https://n8n-corner-pxl.mindtech.studio/webhook/18bfde36-fd7d-48c9-a257-64ee9cfab615", // PROD
+  N8N_DOWNLOAD_WEBHOOK_URL: "https://n8n-corner-pxl.mindtech.studio/webhook/6fca094c-0db6-4d2e-b602-4221addf6035" // PROD
 };
